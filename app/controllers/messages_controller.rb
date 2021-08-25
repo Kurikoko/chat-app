e31070d930159d1d2a58b4d9e11c7ee7d54cf770
Kurikoko <zomahyon@gmail.com>
2021-08-25 19:21:21 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,29 @@
 class MessagesController < ApplicationController
+ 
   def index
+    @message = Message.new
+    # ルーティングのネストから親Roomモデルのidパラメーター取得し、そのidのレコードのみインスタンス変数へ格納
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    # indexのコメントと同様
+    @room = Room.find(params[:room_id])
+    #ルーム内コメントパラメーターを取得
+    @message = @room.messages.new(message_params)
+    # コメントを保存できれば、そのまま、できなかたら入力情報保持したままそのまま
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else 
+      render :index
+    end
+  end
+
+  private
+    # メッセージフォームのcontentをストロングパラメーター設定
+    # ログインしているユーザーに紐付いたcontentを取得できるよう許可
+    def message_params
+      params.require(:message).permit(:content).merge(user_id: current_user.id)
+    end
+
 end
